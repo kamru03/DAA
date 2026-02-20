d7c3e58ed200404099e4f54e17c2de17571cfc04
kamru03 <gitkamru@gmail.com>
2026-02-20 22:03:26 +0530

    ii

--- 4th class/Program 01/main.c
+++ 4th class/Program 01/main.c
@@ -1,49 +1,54 @@
 #include <stdio.h>
 
-int main() {
-    int n, capacity;
-    printf("Enter number of items: ");
-    scanf("%d", &n);
+struct Item {
+    int weight;
+    int profit;
+    float ratio;
+};
 
-    int profit[n], weight[n];
-    float ratio[n];
-
-    for(int i=0; i<n; i++) {
-        printf("Enter profit and weight: ");
-        scanf("%d %d", &profit[i], &weight[i]);
-        ratio[i] = (float)profit[i] / weight[i];
-    }
-
-    printf("Enter capacity: ");
-    scanf("%d", &capacity);
-
-    for(int i=0; i<n-1; i++) {
-        for(int j=i+1; j<n; j++) {
-            if(ratio[i] < ratio[j]) {
-                float temp = ratio[i];
-                ratio[i] = ratio[j];
-                ratio[j] = temp;
-
-                int t = profit[i];
-                profit[i] = profit[j];
-                profit[j] = t;
-
-                t = weight[i];
-                weight[i] = weight[j];
-                weight[j] = t;
+void sort(struct Item arr[], int n) {
+    struct Item temp;
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j].ratio < arr[j+1].ratio) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
+}
+
+int main() {
+    int n, capacity;
+
+    printf("Enter number of items: ");
+    scanf("%d", &n);
+
+    struct Item arr[n];
+
+    for(int i = 0; i < n; i++) {
+        printf("Enter weight and profit of item %d: ", i+1);
+        scanf("%d %d", &arr[i].weight, &arr[i].profit);
+        arr[i].ratio = (float)arr[i].profit / arr[i].weight;
+    }
+
+    printf("Enter capacity of knapsack: ");
+    scanf("%d", &capacity);
+
+    sort(arr, n);
 
     int totalProfit = 0;
-    for(int i=0; i<n; i++) {
-        if(weight[i] <= capacity) {
-            totalProfit += profit[i];
-            capacity -= weight[i];
+    int remainingCapacity = capacity;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i].weight <= remainingCapacity) {
+            totalProfit += arr[i].profit;
+            remainingCapacity -= arr[i].weight;
         }
     }
 
-    printf("Total Profit = %d", totalProfit);
+    printf("\nTotal Profit (Greedy 0/1): %d\n", totalProfit);
 
     return 0;
 }
